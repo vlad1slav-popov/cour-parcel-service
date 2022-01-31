73956bac3dd5104006758db8b1d34e6f663c1c95
vlad1slav-popov <vladpopov9696@gmail.com>
2022-01-31 22:21:11 +0400

    initial commit

---
+++ src/test/java/com/api/courierparcelservice/CourierParcelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.courierparcelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierParcelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
